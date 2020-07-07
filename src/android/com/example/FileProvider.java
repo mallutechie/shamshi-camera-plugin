a6b269929cdc06e722d829574b58dee8d1cdb5f0
Shamsheer <shamsheer@sanesquare.com>
2020-07-07 16:35:20 +0530

    removed unwanted codes

--- src/android/com/example/FileProvider.java
+++
@@ -1,3 +0,0 @@
-package com.example;
-
-public class FileProvider extends android.support.v4.content.FileProvider {}
--- www/plugin.js
+++ www/plugin.js
@@ -4,12 +4,6 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'MyCordovaPlugin';
 
 var MyCordovaPlugin = {
-  echo: function(phrase, cb) {
-    exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
-  },
-  getDate: function(cb) {
-    exec(cb, null, PLUGIN_NAME, 'getDate', []);
-  },
   openCamera:function(cb){
     console.log('Starting camera......')
     exec(cb, null, PLUGIN_NAME, 'openCamera', []);
