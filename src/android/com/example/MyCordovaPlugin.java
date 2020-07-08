3d0fc9eb18a7596dfa59f22b7d39052a32210fda
Shamsheer <shamsheer@sanesquare.com>
2020-07-08 17:34:11 +0530

    cancel

--- src/android/com/example/MyCordovaPlugin.java
+++ src/android/com/example/MyCordovaPlugin.java
@@ -23,6 +23,7 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Field;
 import org.apache.cordova.file.FileUtils;
 import org.apache.cordova.file.LocalFilesystemURL;
+import android.app.Activity;
 
 public class MyCordovaPlugin extends CordovaPlugin {
   private static final String TAG = "MyCordovaPlugin";
@@ -37,7 +38,8 @@ public class MyCordovaPlugin extends CordovaPlugin {
     if(action.equals("openCamera")) {
       Intent intent= new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
       intent.putExtra("android.intent.extra.durationLimit",args.getInt(0));
-      cordova.startActivityForResult((CordovaPlugin)this, intent, 2 * 16 + 1 + 1);
+      Intent chooser=Intent.createChooser(intent,"Test");
+      cordova.startActivityForResult((CordovaPlugin)this, chooser, 2 * 16 + 1 + 1);
       PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
       pluginResult.setKeepCallback(true);
       callback = callbackContext;
@@ -50,44 +52,51 @@ public class MyCordovaPlugin extends CordovaPlugin {
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) 
   {    
-    this.cordova.getThreadPool().execute(new Runnable() {
-      @Override
-      public void run() {
-        String urlPath="URI:"+data.getData().toString();
-        try{
-          File fp = webView.getResourceApi().mapUriToFile(data.getData());
-          PluginManager pm = null;
-          Class webViewClass = webView.getClass();
-          try {
-              Method gpm = webViewClass.getMethod("getPluginManager");
-              pm = (PluginManager) gpm.invoke(webView);
-          } catch (NoSuchMethodException e) {
-          } catch (IllegalAccessException e) {
-          } catch (InvocationTargetException e) {
-          }
-          if (pm == null) {
+    if (resultCode == Activity.RESULT_OK) {
+      this.cordova.getThreadPool().execute(new Runnable() {
+        @Override
+        public void run() {
+          String urlPath="URI:"+data.getData().toString();
+          try{
+            File fp = webView.getResourceApi().mapUriToFile(data.getData());
+            PluginManager pm = null;
+            Class webViewClass = webView.getClass();
             try {
-                Field pmf = webViewClass.getField("pluginManager");
-                pm = (PluginManager)pmf.get(webView);
-            } catch (NoSuchFieldException e) {
+                Method gpm = webViewClass.getMethod("getPluginManager");
+                pm = (PluginManager) gpm.invoke(webView);
+            } catch (NoSuchMethodException e) {
             } catch (IllegalAccessException e) {
+            } catch (InvocationTargetException e) {
             }
-          }
-          try {
-            FileUtils filePlugin = (FileUtils) pm.getPlugin("File");
-            LocalFilesystemURL url = filePlugin.filesystemURLforLocalPath(fp.getAbsolutePath());
-            urlPath=url.toString();
-          } catch (Exception e) {
+            if (pm == null) {
+              try {
+                  Field pmf = webViewClass.getField("pluginManager");
+                  pm = (PluginManager)pmf.get(webView);
+              } catch (NoSuchFieldException e) {
+              } catch (IllegalAccessException e) {
+              }
+            }
+            try {
+              FileUtils filePlugin = (FileUtils) pm.getPlugin("File");
+              LocalFilesystemURL url = filePlugin.filesystemURLforLocalPath(fp.getAbsolutePath());
+              urlPath=url.toString();
+            } catch (Exception e) {
+              urlPath=e.getMessage();
+            }
+          }catch(Exception e){
             urlPath=e.getMessage();
           }
-        }catch(Exception e){
-          urlPath=e.getMessage();
+          PluginResult result = new PluginResult(PluginResult.Status.OK, urlPath);
+          result.setKeepCallback(true);
+          callback.sendPluginResult(result);
         }
-        PluginResult result = new PluginResult(PluginResult.Status.OK, urlPath);
-        result.setKeepCallback(true);
-        callback.sendPluginResult(result);
-      }
-    });
+      });
+    } else if (resultCode == Activity.RESULT_CANCELED) {
+      PluginResult result = new PluginResult(PluginResult.Status.OK, "Cancelled");
+      result.setKeepCallback(true);
+      callback.sendPluginResult(result);
+    }
+    
   }
 
 }
